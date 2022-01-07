87f2af799cb028366f504acbe4560f26eab80fb7
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-08 03:56:14 +0600

    Setting a Static Web Server

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!!!</h1>");
-});
+// app.get("/", (req, res) => {
+//   // res.send("<h1>Hello World!!!</h1>");
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
