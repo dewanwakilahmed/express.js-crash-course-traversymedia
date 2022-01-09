5613094ac2a4eaa50b68f828b42d5a849ff003ca
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-10 01:27:54 +0600

    Update Member

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { route } = require("express/lib/application");
 const uuid = require("uuid");
 const router = express.Router();
 const members = require("../../Members");
@@ -31,8 +32,28 @@ router.post("/", (req, res) => {
     return res.status(400).json({ msg: "Please include a name and an email!" });
   }
 
+  // members.save(newMember); // save to a database
   members.push(newMember);
   res.json(members);
+});
+
+// Update Member
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+
+        res.json({ msg: "Member updated!", member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `Member with id ${req.params.id} not found!` });
+  }
 });
 
 module.exports = router;
